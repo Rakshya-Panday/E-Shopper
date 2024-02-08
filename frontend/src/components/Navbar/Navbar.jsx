c9ab6a73ebc702dee883e88bd62cb0488120b4af
Rakshya-Panday <rakshyapanday202@gmail.com>
2024-02-08 12:11:16 +0545

    updated

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../Assests/logo.jpg'
+import cart from '../Assests/cart.jpg'
 
 const Navbar = () => {
   return (
@@ -9,9 +10,25 @@ const Navbar = () => {
             <img src= {logo} alt="logo" />
             <p>SHOPPER</p>
         </div>
+       
+          <ul className='nav-menu'>
+            <li>Shop</li>
+          
+            <li>Men</li>
+            <li>Female</li>
+            <li>Kids</li>
+            </ul>
+        
+        <div className='nav-login-cart'>
+          <button>Login</button>
+          <img src={cart} alt="cart" />
 
 
+        </div>
+
+        <hr />
     </div>
+    
   )
 }
 
