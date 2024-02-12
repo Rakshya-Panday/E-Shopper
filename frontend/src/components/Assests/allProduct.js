80f070125b2b3bf112039f274f187aac4a45981b
Rakshya-Panday <rakshyapanday202@gmail.com>
2024-02-12 14:09:58 +0545

    Adding the context file of shop

---
+++ frontend/src/components/Assests/allProduct.js
@@ -0,0 +1,113 @@
+import kid1 from '../Assests/kid1.jpg';
+import kid2 from '../Assests/kid2.jpg';
+import kid3 from '../Assests/kid3.jpg';
+import kid4 from '../Assests/kid4.jpg';
+import women1 from '../Assests/women1.jpg';
+import women2 from '../Assests/women2.jpg';
+import women3 from '../Assests/women3.jpg';
+import women4 from '../Assests/women4.jpg';
+import men1 from '../Assests/men1.jpg';
+import men2 from '../Assests/men2.jpg';
+import men3 from '../Assests/men3.jpg';
+import men4 from '../Assests/men4.jpg';
+
+const allProduct = [
+    {
+        id:1,
+        name :"kid dress",
+        category : "kid",
+        image : kid1,
+        new_price :85.0,
+        old_price :90.8
+    },
+    {
+        id:2,
+        name :"kid dress",
+        category : "kid",
+        image : kid2,
+        new_price :85.0,
+        old_price :90.8
+    },
+    {
+        id:3,
+        name :"kid dress",
+        category : "kid",
+        image : kid3,
+        new_price :85.0,
+        old_price :90.8
+    },
+    {
+        id:4,
+        name :"kid dress",
+        category : "kid",
+        image : kid4,
+        new_price :85.0,
+        old_price :90.8
+    },
+    {
+        id:5,
+        name :"Women dress",
+        category : "women",
+        image : women1,
+        new_price :85.0,
+        old_price :90.8
+    },
+    {
+        id:6,
+        name :"Women dress",
+        category : "women",
+        image : women2,
+        new_price :85.0,
+        old_price :90.8
+    },
+    {
+        id:7,
+        name :"Women dress",
+        category : "women",
+        image : women3,
+        new_price :85.0,
+        old_price :90.8
+    },
+    {
+        id:8,
+        name :"Women dress",
+        category : "women",
+        image : women4,
+        new_price :85.0,
+        old_price :90.8
+    },
+    {
+        id:9,
+        name :"Men dress",
+        category : "men",
+        image : men1,
+        new_price :85.0,
+        old_price :90.8
+    },
+    {
+        id:10,
+        name :"Men dress",
+        category : "men",
+        image : men2,
+        new_price :85.0,
+        old_price :90.8
+    },
+    {
+        id:11,
+        name :"Men dress",
+        category : "men",
+        image : men3,
+        new_price :85.0,
+        old_price :90.8
+    },
+    {
+        id:12,
+        name :"Men dress",
+        category : "men",
+        image : men4,
+        new_price :85.0,
+        old_price :90.8
+    },
+];
+
+export default allProduct;
